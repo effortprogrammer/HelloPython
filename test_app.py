dab199f7ddf20347bf259f494b6cf3d4ec5e72ef
program <57383540+effortprogrammer@users.noreply.github.com>
2020-08-11 19:59:07 +0900

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_get_msg(self):
+        self.assertEquals
+
+if __name__ == '__main__':
+    unittest.main()
